51cee00ecb6d253f3f99ae17534ec6cb75ba007b
Jason M <jlmerri1@neiu.edu>
2017-04-08 06:45:17 -0500

    add files via upload

---
+++ bindingWPF/bindingWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bindingWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<ToDoItem> items = new List<ToDoItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            items.Add(new ToDoItem() { Title = "Complete grading", Progress = 85 });
+            items.Add(new ToDoItem() { Title = "Learn binding", Progress = 10 });
+            items.Add(new ToDoItem() { Title = "Finsih class", Progress = 15 });
+
+            ListToDo.ItemsSource = items;
+        }
+    }
+
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public int Progress { get; set; }
+    }
+}
---
+++ buttons/buttons/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace buttons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //btn.ClickMode = ClickMode.Press;
+            btn.ClickMode = ClickMode.Hover;
+            tggleBtn.IsThreeState = true;
+            chckBox1.IsThreeState = true;
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button thisButton = (Button)sender;
+            //or btn....
+
+            txt1.Text = thisButton.IsPressed.ToString();
+        }
+
+        private void rBtn_Click(object sender, RoutedEventArgs e)
+        {
+            pBar.Value += 1;
+        }
+
+        private void tggleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (chckBox1.IsChecked == true)
+                pBar2.Value = 100;
+            else if (chckBox1.IsChecked == false)
+                pBar2.Value = 0;
+            else
+                pBar2.Value = 50;
+
+        }
+
+        
+    }
+}
