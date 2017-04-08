84f11765603fc7465c2e49440bbb9ec1ba8ffea8
Jason M <jlmerri1@neiu.edu>
2017-04-08 06:48:18 -0500

    Add files via upload

---
+++ StockProgram/StockProgram/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockProgram
+{
+    public class StockEventArgs : EventArgs  //sending data to the subscribers
+    {
+        public float googleStock;
+        public float amazonStock;
+        public float telsaStock;
+
+        public StockEventArgs(float g, float a, float t)
+        {
+            this.googleStock = g;
+            this.amazonStock = a;
+            this.telsaStock = t;
+        }
+    }
+
+
+    public class Stock
+    {
+        private float googleStock;
+        private float amazonStock;
+        private float teslaStock;
+
+        public delegate void StocksChangedHandler(object stock, StockEventArgs s);
+        public StocksChangedHandler StockChanged;
+
+        public void ReceiveStock()
+        {
+            Random rand = new Random();
+            //randomlly generate stock price
+            //subscribers are modified
+            googleStock = (float) rand.Next(1000);
+            amazonStock = (float)rand.Next(1000);
+            teslaStock = (float)rand.Next(1000);
+
+            Console.WriteLine("Stocks Received: ");
+            Console.WriteLine("Google: " + googleStock + " Amazon: " + amazonStock + " Tesla: " + teslaStock);
+
+            StockEventArgs stock = new StockEventArgs(googleStock, amazonStock, teslaStock);
+
+            if(StockChanged != null)
+            {
+                StockChanged(this, stock);
+            }
+        }
+    }
+
+    public class ItStockBuyer
+    {
+        private float gStock;
+        private float aStock;
+
+        public void Subscribe(Stock stock)
+        {
+            stock.StockChanged += new Stock.StocksChangedHandler(BuyStock); //+= means we add a method to the delegates list
+        }
+
+        public void BuyStock(object stock, StockEventArgs s)
+        {
+            Console.WriteLine("Buyer: New Stock List.");
+            Console.WriteLine("Google: " + s.googleStock + " Amazon: " + s.amazonStock);
+
+            if (s.googleStock > 500)
+            {
+                Console.WriteLine("Buying google stock.");
+            }
+            else
+                Console.WriteLine("Not buying google stock.");
+            if(s.amazonStock > 300)
+            {
+                Console.WriteLine("Buying amazon stock.");
+            }
+            else
+                Console.WriteLine("Not buying amazon stock.");
+        }
+    }
+
+    public class ItStockSeller
+    {
+        private float tStock;
+
+        public void SellStock()
+        {
+
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock stock = new Stock();
+            ItStockBuyer buyer = new ItStockBuyer();
+
+            buyer.Subscribe(stock);
+
+            stock.ReceiveStock();
+
+            Console.Read();
+        }
+    }
+}
---
+++ TicketProgram/TicketProgram/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketProgram
+{
+    public class TicketInfoArgs : EventArgs
+    {
+        public DateTime movieDate;
+        public string movieName;
+
+        public TicketInfoArgs(DateTime d, string m)
+        {
+            this.movieDate = d;
+            this.movieName = m;
+        }
+    }
+
+    public class MovieTickets
+    {
+        public delegate void MovieChangedHandler(object movie, TicketInfoArgs t);
+        public MovieChangedHandler MovieChanged;
+
+        public void PurchaseTickets(DateTime movieDate, string movieName)
+        {
+            TicketInfoArgs ticket = new TicketInfoArgs(movieDate, movieName);
+
+            if(MovieChanged != null)
+            {
+                MovieChanged(this, ticket);
+            }
+        }
+    }
+
+    public class TicketSales
+    {
+        public void Subscribe(MovieTickets tickets)
+        {
+            tickets.MovieChanged +=  new MovieTickets.MovieChangedHandler(NewPurchase);
+        }
+
+        public void NewPurchase(object movie, TicketInfoArgs t)
+        {
+            Console.WriteLine("Movie date: " + t.movieDate);
+            Console.WriteLine("Movie name: " + t.movieName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieTickets mt = new MovieTickets();
+            TicketSales ts = new TicketSales();
+            ts.Subscribe(mt);
+            mt.PurchaseTickets(new DateTime(2016, 3, 25), "Split");
+            Console.Read();
+
+        }
+    }
+}
